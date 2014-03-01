99d91a9074f9c2b499117b110ebdf9e399c19db7
Edwin Hollen <edwinhollen@gmail.com>
2014-03-01 11:43:47 -0500

    Added dice functions.

--- src/Chance.java
+++ src/Chance.java
@@ -301,4 +301,30 @@ public class Chance {
         }
 
     }
+
+    // -- Dice --
+    public int d4(){
+        return this.integer(1,4);
+    }
+    public int d6(){
+        return this.integer(1,6);
+    }
+    public int d8(){
+        return this.integer(1,8);
+    }
+    public int d10(){
+        return this.integer(1,10);
+    }
+    public int d12(){
+        return this.integer(1,12);
+    }
+    public int d20(){
+        return this.integer(1,20);
+    }
+    public int d30(){
+        return this.integer(1,30);
+    }
+    public int d100(){
+        return this.integer(1,100);
+    }
 }
